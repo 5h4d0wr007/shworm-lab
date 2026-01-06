59f03613ac8fd9d5538290f00bd33f525933a838
Anurag Mewar <51066119+5h4d0wr007@users.noreply.github.com>
2026-01-06 14:00:07 +0530

    Create index.test.js

---
+++ packages/core-logger/index.test.js
@@ -0,0 +1,7 @@
+const logger = require("./index");
+
+logger.logInfo("core-logger test info");
+logger.logWarn("core-logger test warn");
+logger.logError("core-logger test error");
+
+console.log("core-logger tests completed.");
